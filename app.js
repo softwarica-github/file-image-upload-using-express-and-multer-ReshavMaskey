1b695ef316048d12edf57aafbc7389a74caf77e8
“reshav401” <reshav401@gmail.com>
2019-04-26 20:17:24 +0545

    upload code added

--- app.js
+++ app.js
@@ -17,30 +17,64 @@ app.get("/main", function(req, res) {
   res.render("index");
 });
 
-var imgUpload = multer.diskStorage({
-  destination: function(req, file, des) {
-    console.log("Img upload multer");
-    des(null, "res/uploads");
+///////////////////////////////////////////////
+
+//Define where project photos will be stored
+var storage = multer.diskStorage({
+  destination: function(request, file, callback) {
+    callback(null, "res/uploads");
   },
-  filename: function(req, file, des) {
-    des(null, "image" + req.file);
+  filename: function(request, file, callback) {
+    console.log(file);
+    callback(null, file.originalname);
   }
 });
-var upload = multer({
-  storage: imgUpload
-});
-app.post("/upload", upload.single("upload"), function(req, res, next) {
-  console.log(req.body);
-  console.log(req.body.upload.filename + " file added");
+
+// Function to upload project images
+var upload = multer({ storage: storage });
+
+// add new photos to the DB
+// app.post("/projects", function(req, res) {
+//   upload(req, res, function(err) {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log(req.files);
+//     res.end("Your files uploaded.");
+//     console.log("Yep yep!");
+//   });
+// });
+//////////////////////////////////////
+
+// var imgUpload = multer.diskStorage({
+//   destination: function(req, file, des) {
+//     console.log("Img upload multer");
+//     des(null, "res/uploads");
+//   },
+//   filename: function(req, file, des) {
+//     des(null, "image.jpg");
+//   }
+// });
+// var upload = multer({
+//   storage: imgUpload
+// });
+// app.post("/upload", upload.single("upload"), function(req, res, next) {
+//   console.log(req.body);
+// });
+app.post("/upload", upload.array("upload"), function(req, res, next) {
+  var file = req.files;
+  console.log(file);
+
+  res.end();
 });
 // app.post("/upload", function(req, res) {
-//   res.render("index");
-//   // res.json({
-//   //   name: req.body.name,
-//   //   email: req.body.email,
-//   //   address: req.body.address,
-//   //   feedback: req.body.feedback,
-//   //   file: req.body.uploadedFile
-//   // });
+//   res.json({
+//     name: req.body.name,
+//     email: req.body.email,
+//     address: req.body.address,
+//     feedback: req.body.feedback,
+//     file: req.body.upload
+//   });
 // });
 app.listen(process.env.PORT);
